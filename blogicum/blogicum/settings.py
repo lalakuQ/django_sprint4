d007e392a4b66902bd7fbee6c89b6f2be42dc452
lalakuQ <vdyugay2004@gmail.com>
2024-05-03 22:26:08 +0500

    refactor

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c9=bv-xng1ukfdnrj3rx*co=(o=_(-4!lv)_c&h9g5#8@&uhzq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
